686b1c738cadd71226d848715de7b668e4c211d1
petitsurume <git@surume.tk>
2017-07-12 14:07:27 +0900

    wip

---
+++ generator/index.js
@@ -0,0 +1,7 @@
+const pug = require("pug")
+const yaml = require("yaml")
+const fs = require("fs")
+const md = require("markdown")
+const path = require("path")
+
+DATA_DIR = path.join(__dirname, "..", "data")
